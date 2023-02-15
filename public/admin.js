d6d8bfcc97387cab05fadb78570fde1bb7997fd1
George Leyva <leyvageorge@hotmail.com>
2023-02-15 11:48:35 -0800

    changes unkown to admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,24 @@
 
 // Your Code Here
+let response = await fetch('http://127.0.0.1:3000/listBooks', {
+    method: "GET",
+    headers: null,
+    body: null
+});
+
+let books = await response json();
+console.log(books);
+
+let response = await fetch('http://127.0.0.1:3000/updateBook', {
+    methode: "PATCH",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({
+        "id": 3,
+        "title" : "Legends of Arathrae",
+    }),
+});
+
+let updateBook = await response.json();
+console.log(updateBook)
